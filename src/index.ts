1c596743b538f242793cf30c0dc91b3680b523ee
Tarak <tarak.suthar@gmail.com>
2022-01-24 17:10:19 +0530

    refund added

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express,{Router} from 'express'
+import express from 'express'
 import dotenv from 'dotenv'
 import paymentRouter from './routes/payment.route'
 import bodyParser from 'body-parser'
@@ -7,44 +7,51 @@ const app = express()
 dotenv.config();
 const port = process.env.PORT||3090
 
-app.post('/webhook',bodyParser.raw({ type: "application/json" }),(request, response) => {
+app.post('/webhook',bodyParser.raw({ type: "application/json" }),async(request, response) => {
   const endpointSecret = "whsec_eOcIUONdzUfhkNz0Q2ikfET9m0lwrI5V";
   const sig = request.headers['stripe-signature']?request.headers['stripe-signature']:'';
   let event
   let paymentIntent:any
   try {
     event = Stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+  
+      switch (event.type) {
+        case 'payment_intent.canceled':
+          paymentIntent = event.data.object;
+          // Then define and call a function to handle the event payment_intent.canceled
+          break;
+        case 'payment_intent.created':
+          paymentIntent = event.data.object;
+          
+          // Then define and call a function to handle the event payment_intent.created
+          break;
+        case 'payment_intent.succeeded':
+          paymentIntent = event.data.object;
+          console.log('payment success',paymentIntent)
+          let qty = Number(paymentIntent.amount_received/1000)
+          const deliveredQuantity:number = getDeliveredQuantity(qty)
+          if(deliveredQuantity<qty){
+            console.log('----------------------initiate refund---------------------------------')
+            console.log('refund amount',deliveredQuantity*1000)
+              const refund = await Stripe.refunds.create({
+                payment_intent: paymentIntent.id,
+                amount: deliveredQuantity*1000,
+              });
+              console.log('refund success',refund)
+
+          }
+          break;
+        // ... handle other event types
+        default:
+          console.log(`Unhandled event type ${event.type}`);
+      }
+
+  // Return a 200 response to acknowledge receipt of the event
+    response.send();
   } catch (err:any) {
     response.status(400).send(`Webhook Error: ${err.message}`);
     return;
   }
-  switch (event.type) {
-    case 'payment_intent.canceled':
-      paymentIntent = event.data.object;
-      // Then define and call a function to handle the event payment_intent.canceled
-      break;
-    case 'payment_intent.created':
-      paymentIntent = event.data.object;
-      
-      // Then define and call a function to handle the event payment_intent.created
-      break;
-    case 'payment_intent.succeeded':
-      paymentIntent = event.data.object;
-      console.log('payment success')
-      let qty = Number(paymentIntent.amount_received/1000)
-      const deliveredQuantity:number = getDeliveredQuantity(qty)
-      if(deliveredQuantity<qty){
-        console.log('----------------------initiate refund---------------------------------')
-        console.log('refund amount',deliveredQuantity*1000)
-      }
-      break;
-    // ... handle other event types
-    default:
-      console.log(`Unhandled event type ${event.type}`);
-  }
-
-  // Return a 200 response to acknowledge receipt of the event
-  response.send();
 });
 
 app.use(bodyParser.json())
